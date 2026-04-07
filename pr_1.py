04197f7cb750c41c68f365924807ce024cce2f53
Владос царь /. <154845500+RageDomination@users.noreply.github.com>
2026-04-07 13:30:53 +0300

    Додав додатковi коментарi до коду

--- pr_1.py
+++ pr_1.py
@@ -2,25 +2,28 @@ import tkinter as tk
 
 tasks = []
 
+# функцiя додавання
 def add_task():
     task = entry.get()
     tasks.append(task)
     listbox.insert(tk.END, task)
 
+# функцiя видалення
 def delete_task():
     selected = listbox.curselection()
     for i in reversed(selected):
         listbox.delete(i)
 
+# функцiя стерти все
 def clear_tasks():
     length = listbox.size()
     for i in range(length // 2):
         listbox.delete(0)
 
 root = tk.Tk()
-root.title("My App")
+root.title("My App") # назва вiкна
 
-root.geometry("200x150")
+root.geometry("200x150") # розмi стартового вiкна
 
 entry = tk.Entry(root)
 entry.pack()
